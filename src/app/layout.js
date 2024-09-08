4581390c87defc7910319e79982bbb28390f8352
Esmeralda Gonzalez <esmeraldamenera27@gmail.com>
2024-09-08 18:42:38 -0400

    add header component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import Header from "./components/Header";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -21,11 +22,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          <Header/>
         {children}
       </body>
     </html>
   );
 }
-
