f47d0b9a0f236869f074cb7047280f30f6b43eab
Esmeralda Gonzalez <esmeraldamenera27@gmail.com>
2024-09-28 23:38:41 -0400

    complete the MiniProfile component

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,9 @@
-import React from "react";
 import Feed from "@/components/Feed";
 
 export default function Home() {
-  <>
-    <Feed/>
-  </>
+  return (
+    <>
+      <Feed />
+    </>
+  );
 }
--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,19 +1,20 @@
 import Posts from "./Posts";
 import MiniProfile from "./MiniProfile";
 
-
 export default function Feed() {
-    return (
-        <main className="grid grid-cols-1 md:grid-cols-3 md:max-w-6xl  mx-auto">
-            {/* posts (left) */}
-            <section className="md:col-span-2">
-                <Posts/>
-            </section>
+  return (
+    <main className="grid grid-cols-1 md:grid-cols-3 md:max-w-6xl  mx-auto">
+      {/* posts (left) */}
+      <section className="md:col-span-2">
+        <Posts />
+      </section>
 
-            {/* MiniProfile (right) */}
-            <section className="hidden md:inline-grid md:col-span-1"> 
-                <MiniProfile/>
-            </section>
-        </main>
-    )
+      {/* MiniProfile (right) */}
+      <section className="hidden md:inline-grid md:col-span-1">
+        <div className="fixed w-[380px]">
+          <MiniProfile />
+        </div>
+      </section>
+    </main>
+  );
 }
