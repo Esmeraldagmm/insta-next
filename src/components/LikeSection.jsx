c1919fdc1a1b4e42bdb69fb63ad331a0677f5575
Esmeralda Gonzalez <esmeraldamenera27@gmail.com>
2024-10-03 02:37:22 -0400

    add like functionality to the post card

---
+++ src/components/LikeSection.jsx
@@ -0,0 +1,65 @@
+ "use client"
+
+ import { useSession } from "next-auth/react";
+ import { HiOutlineHeart, HiHeart } from "react-icons/hi";
+ import { useEffect, useState } from "react";
+ import { collection, onSnapshot, getFirestore, doc, deleteDoc, setDoc } from "firebase/firestore";
+ import {app} from "../firebase";
+ 
+ export default function LikeSection({ id }) {
+    const { data: session } = useSession();
+    const [hasLiked, setHasLiked] = useState(false);
+    const [likes, setLikes] = useState([]);
+    const db = getFirestore(app);
+
+    useEffect(() => {
+        onSnapshot(collection(db, "posts", id, "likes"), 
+        (snapshot) => {
+            setLikes(snapshot.docs);
+        });
+    }, [db]);
+
+    useEffect(() => {
+        if (likes.findIndex((like) => like.id === session?.user?.uid) !== -1) {
+            setHasLiked(true);
+        } else {
+            setHasLiked(false);
+        }
+    }, [likes]);
+
+    async function likedPost() {
+        if (hasLiked) {
+            await deleteDoc(doc(db, "posts", id, "likes", session?.user?.uid));
+        } else {
+            await setDoc(doc(db, "posts", id, "likes", session?.user?.uid), {
+                username: session?.user?.username,
+            });
+        }
+    }
+
+
+   return (
+     <div>
+       {session && (
+        <div className="flex border-t border-gray-100 px-4 pt-4">
+            <div className="flex items-center">
+                {hasLiked ? (
+                    <HiHeart 
+                    onClick={likedPost}
+                    className="text-red-500 cursor-pointer text-3xl hover:scale-125 transition-transform duration-200 ease-out"/>
+                ) : (
+                    <HiOutlineHeart
+                    onClick={likedPost}
+                    className="cursor-pointer text-3xl hover:scale-125 transition-transform duration-200 ease-out"/>
+                )}
+                {likes.length > 0 && (
+                    <p className="text-black font-semibold pl-1">
+                        {likes.length}{ likes.length === 1 ? " like" : " likes"}
+                    </p>
+                )}
+            </div>
+        </div>
+       )}
+     </div>
+   );
+ }
